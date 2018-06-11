31f30e033c1a967a812e1f5ac039a026c4736ce9
zkolodny <zbkolodny@me.com>
2018-06-11 00:05:06 -0400

    updated readme

--- bulk_upload/Spring17/CMSC/project7/test_code.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for x in range(1,6):
-    print('-' * 80)
-    print("PUBLIC TEST: " + str(x))
-    os.system("gcc public0" + str(x) + ".c my_memory_checker_216.c calendar.c -0 a.out")
-    os.system("./a.out | diff -u - public0" + str(x) + ".output")
-print('-' * 80)
